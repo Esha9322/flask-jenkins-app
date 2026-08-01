b221b04fcac9284f2f75eaf45cdcb798fec3e756
Esha9322 <eshavasale99@gmail.com>
2026-08-01 16:09:14 +0530

    Updated home page to Version 2

--- app.py
+++ app.py
@@ -5,14 +5,12 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return """
-    <h1>🚀 Welcome to Jenkins!</h1>
+    <h1>🚀 Welcome to Jenkins CI/CD</h1>
     <h2>Hello Esha 👋</h2>
-    <p>This Flask application is running successfully.</p>
+    <p>Version 2.0</p>
+    <p>Application updated successfully.</p>
     """
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
-
-
 
